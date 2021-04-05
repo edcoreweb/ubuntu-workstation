bd81eef755a0a0df7087fdb76002e404ed2bd99c
Eduard Tofan <edcoreweb@gmail.com>
2021-04-06 02:22:46 +0300

    WIP

--- install.ps1
+++ install.ps1
@@ -1,19 +1,31 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [String]$Version,
+
+    [Parameter(Mandatory=$true)]
+    [String]$VMName,
+
+    [Parameter(Mandatory=$true)]
+    [String]$VMPath
+)
+
 # Global settings
 $ProgressPreference = 'SilentlyContinue'
 
-$version = $args[0]
+$version = $Version
 $root = $PSScriptRoot
 $path = "${root}\temp"
 $bin = "${root}\bin"
 $releasesUrl = "https://releases.ubuntu.com"
 
-if ("18.04", '20.04' -notcontains $version) {
+if ('20.04' -notcontains $version) {
     throw "Invalid Ubuntu version [$version]."
 }
 
 function Find-Release {
     Param ($MajorVersion)
-    # ubuntu-18.04.5-live-server-amd64.iso
+    # ubuntu-20.04.5-live-server-amd64.iso
     $html = (Invoke-WebRequest "${releasesUrl}/${MajorVersion}" -UseBasicParsing).rawcontent
 
     if ($html -match "ubuntu-${MajorVersion}(.*)-live-server-amd64.iso") {
@@ -57,31 +69,42 @@ New-Item -Path "${path}\unattended\nocloud\meta-data" -ItemType "file" -Force | 
 Copy-Item "${root}\${version}\user-data.yml" -Destination "${path}\unattended\nocloud\user-data" -Force | Out-Null
 Copy-Item "${root}\${version}\txt.cfg" -Destination "${path}\unattended\isolinux\txt.cfg" -Force | Out-Null
 Copy-Item "${root}\${version}\grub.cfg" -Destination "${path}\unattended\boot\grub\grub.cfg" -Force | Out-Null
-Copy-Item "${root}\${version}\loopback.cfg" -Destination "${path}\unattended\boot\grub\loopback.cfg" -Force | Out-Null
+
+# By-Pass MD5 check
+$hash = (Get-FileHash -Path "${path}\unattended\.disk\info" -Algorithm MD5).Hash
+Set-Content -Path "${path}\unattended\md5sum.txt" -Value "${hash}  ./.disk/info" -Force
 
 # Repackage Image
 Write-Output "Repacking image..."
 $newIsoPath = "${path}\unattended-${name}"
 Remove-Item -Path $newIsoPath -Force -ErrorAction SilentlyContinue | Out-Null
-& "${bin}\xorriso.exe" -as mkisofs -r -V "Ubuntu ${version}" -J -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -input-charset utf-8 -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot -isohybrid-gpt-basdat -o $newIsoPath /temp/unattended
-
-# Make a new VM
-#Write-Output "Instaling a new VM..."
-#$vmName = "Ubuntu Workstation"
-#New-VM -Name $vmName -Generation 2 -MemoryStartupBytes 8GB -Path "D:\Hyper-V\${vmName}" -NewVHDPath "D:\Hyper-V\${vmName}\Disk.vhdx" -NewVHDSizeBytes 80GB
-#Set-VMFirmware -VMName $vmName -EnableSecureBoot Off
-#Add-VMDvdDrive -VMName $vmName -Path $newIsoPath
-#Start-VM -Name $vmName
+& "${bin}\xorriso.exe" -as mkisofs -r -V "Ubuntu ${version}" -J -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -input-charset utf-8 -isohybrid-mbr "/${version}/isohdpfx.bin" -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot -isohybrid-gpt-basdat -o $newIsoPath /temp/unattended 2> $null
 
 # Install a new VM Switch
-#New-VMSwitch -SwitchName "NATSwitch" -SwitchType Internal
+$switch = "NATSwitch"
+If (-Not (Get-VMSwitch -SwitchName $switch -ErrorAction SilentlyContinue)) {
+    Write-Output "Instaling a new NAT adapter..."
+    New-VMSwitch -SwitchName $switch -SwitchType Internal
+    New-NetIPAddress -IPAddress 192.168.20.1 -PrefixLength 24 -InterfaceAlias "vEthernet (${switch})"
+    New-NetNAT -Name "NATNetwork" -InternalIPInterfaceAddressPrefix 192.168.20.0/24
+}
 
-# Assign a subnet
-#New-NetIPAddress -IPAddress 192.168.20.1 -PrefixLength 24 -InterfaceAlias "vEthernet (NATSwitch)"
-#New-NetNAT -Name "NATNetwork" -InternalIPInterfaceAddressPrefix 192.168.20.0/24
+# Make a new VM
+Write-Output "Instaling a new VM..."
+New-VM -Name $VMName -Generation 2 -MemoryStartupBytes 2GB -Path "${VMPath}\${VMName}" -NewVHDPath "${VMPath}\${VMName}\Disk.vhdx" -NewVHDSizeBytes 80GB -SwitchName $switch
+Set-VMProcessor -VMName $VMName -Count 8
+$dvd = Add-VMDvdDrive -VMName $VMName -Path $newIsoPath -Passthru
+Set-VMFirmware -VMName $VMName -EnableSecureBoot Off -FirstBootDevice $dvd
+Start-VM -Name $VMName
 
-# Assign a ip address to the vm
-# TODO
+# Wait for it to stop
+Write-Output "Provisioning the VM..."
+while ((Get-VM -name $VMName).state -eq 'Running') {
+    start-sleep -s 5
+}
+
+Write-Output "Done."
+#Start-VM -Name $VMName
 
 # Port forward 80, 443
 #Add-NetNatStaticMapping -ExternalIPAddress "0.0.0.0/24" -ExternalPort 80 -Protocol TCP -InternalIPAddress "192.168.20.128" -InternalPort 80 -NatName NATNetwork
